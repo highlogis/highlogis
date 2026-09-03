9215df233e8a989adcbb9e598722c9b0a5d53faa
highlogis <highlogis@naver.com>
2026-09-03 13:45:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function autoplay(){clearInterval(timer);timer=setInterval(()=>show(current+1),6
 dots.forEach((d,i)=>d.addEventListener('click',()=>{show(i);autoplay()}));
 autoplay();
 window.addEventListener('scroll',()=>document.querySelector('.header').classList.toggle('scrolled',scrollY>30));
+
+// 메인 홈페이지는 일반 진입/재진입 시 항상 첫 화면에서 시작합니다.
+if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
+function resetHomeTop() {
+  if (!location.hash || location.hash === '#top') {
+    requestAnimationFrame(() => window.scrollTo(0, 0));
+  }
+}
+window.addEventListener('pageshow', resetHomeTop);
+window.addEventListener('load', resetHomeTop);
